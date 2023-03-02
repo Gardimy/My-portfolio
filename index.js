8706866e6279acf2c1697ca53b9cae2dd8db6f15
Gardimy Charles <gardimycharles889@gmail.com>
2023-03-02 14:31:25 -0500

    fix linters and eslint errors

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const option = document.querySelector('.option');
 const openmenu = document.querySelector('.openmenu');
 const closemenu = document.querySelector('.closemenu');
 
-openmenu.addEventListener('click' , showmenu);
-closemenu.addEventListener('click' , close);
-
 function showmenu () {
 	option.style.display = 'flex';
 	openmenu.style.display = 'none';
 }
+openmenu.addEventListener('click', showmenu);
 function close () {
 	option.style.top = '-100%'
 	closemenu.style.display = 'block';
 }
+closemenu.addEventListener('click', close);
