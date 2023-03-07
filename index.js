7f365929629396c948bff602b1cd83aa0f0dcd7e
Gardimy Charles <gardimycharles889@gmail.com>
2023-03-07 13:23:57 -0500

    Add js for the poppup window

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+
 const option = document.querySelector('.option');
 const openmenu = document.querySelector('.openmenu');
 const closemenu = document.querySelector('.closemenu');
 
-function showmenu() {
-  option.style.display = 'flex';
-  openmenu.style.display = 'none';
-}
-openmenu.addEventListener('click', showmenu);
-function close() {
-  option.style.top = '-100%';
-  closemenu.style.display = 'block';
-}
-closemenu.addEventListener('click', close);
+openmenu.addEventListener('click', function(){
+	option.style.display = 'flex';
+	openmenu.style.display ='none';
+});
+
+closemenu.addEventListener('click', function(){
+	option.style.top = '-100%';
+});
+
+
+
---
+++ poppup.js
@@ -0,0 +1,53 @@
+const containersdetails = [
+  {
+	snapshoot: 'Images/Snapshoot1.png',
+	desktop: 'Images/Desktop1.png',
+    Tonic: 'Tonic',
+    offer: ['CANOPY', 'Back End Dev', '2015'],
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    At eos qui possimus nisi ducimus delectus enim beatae natus 
+    ab? Natus minima minus similique nisi eos, deleniti laudantium 
+    nihil impedit assumenda! ducimus delectus enim beatae natus 
+    ab? Natus minima minusbsbusb dkdnojdpckdc dkpkvpdkdfd  djgjwoajsmlncknckdv`,
+    skills: ['HTML', 'css', 'javaScript'],
+
+  },
+  {
+    snapshoot: 'Images/Snapshoot1.png',
+	desktop: 'Images/Desktop1.png',
+    Tonic: 'Tonic',
+    offer: ['CANOPY', 'Back End Dev', '2015'],
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    At eos qui possimus nisi ducimus delectus enim beatae natus 
+    ab? Natus minima minus similique nisi eos, deleniti laudantium 
+    nihil impedit assumenda! ducimus delectus enim beatae natus 
+    ab? Natus minima minusbsbusb dkdnojdpckdc dkpkvpdkdfd  djgjwoajsmlncknckdv`,
+    skills: ['HTML', 'css', 'javaScript'],
+  },
+  {
+    snapshoot: 'Images/Snapshoot1.png',
+	desktop: 'Images/Desktop1.png',
+    Tonic: 'Tonic',
+    offer: ['CANOPY', 'Back End Dev', '2015'],
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    At eos qui possimus nisi ducimus delectus enim beatae natus 
+    ab? Natus minima minus similique nisi eos, deleniti laudantium 
+    nihil impedit assumenda! ducimus delectus enim beatae natus 
+    ab? Natus minima minusbsbusb dkdnojdpckdc dkpkvpdkdfd  djgjwoajsmlncknckdv`,
+    skills: ['HTML', 'css', 'javaScript'],
+  },
+  {
+    snapshoot: 'Images/Snapshoot1.png',
+	desktop: 'Images/Desktop1.png',
+    Tonic: 'Tonic',
+    offer: ['CANOPY', 'Back End Dev', '2015'],
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+    At eos qui possimus nisi ducimus delectus enim beatae natus 
+    ab? Natus minima minus similique nisi eos, deleniti laudantium 
+    nihil impedit assumenda! ducimus delectus enim beatae natus 
+    ab? Natus minima minusbsbusb dkdnojdpckdc dkpkvpdkdfd  djgjwoajsmlncknckdv`,
+    skills: ['HTML', 'css', 'javaScript'],
+  },
+];
+
+
